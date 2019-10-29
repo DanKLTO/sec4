15c3afa92b3a2897c88ce97465b1b3d73f31e7bc
dan <danklto@yahoo.com>
2019-10-29 22:16:34 +0800

    Updates

---
+++ precache-manifest.d50b6009dfd39fae8b2c49e77342dd33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c2e5db4a836efa10be8b8860f2a0a6d",
+    "url": "/sec4/index.html"
+  },
+  {
+    "revision": "8ed8562d6be89d28a888",
+    "url": "/sec4/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "81cd1148717ad33e6acd",
+    "url": "/sec4/static/js/2.0b18a650.chunk.js"
+  },
+  {
+    "revision": "8ed8562d6be89d28a888",
+    "url": "/sec4/static/js/main.0a549425.chunk.js"
+  },
+  {
+    "revision": "4fd2e20a3a3f2bbdd58c",
+    "url": "/sec4/static/js/runtime-main.8536e52d.js"
+  }
+]);
